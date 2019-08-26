364690baa11330142b3e4b70a7e283f6ed287940
Diego Fernando Guiza Galeano <diego.guizag@campusuuc.edu.co>
2019-08-26 11:22:30 -0500

    Conexión Base de datos
    Se realiza conexión a base de datos Firebase y prueba de lista de información.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'proyectoGrado';
+
+  items: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) { }
+
+
+  ngOnInit(): void {
+    this.items = this.db.collection('items').valueChanges();
+        // .subscribe(data => {
+        //   console.log(data);
+        // });
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
