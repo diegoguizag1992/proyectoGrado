b4f3fc1b381947d586e6a6216d3add2ce4f0bc59
Diego Fernando Guiza Galeano <diego.guizag@campusuuc.edu.co>
2019-09-03 15:25:46 -0500

    Rutas
    Creación de las rutas de los componentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+import { RequerimientoComponent } from './components/auth/requerimientos/requerimiento/requerimiento.component';
+import { RequerimientosComponent } from './components/auth/requerimientos/requerimientos.component';
+import { PrincipalComponent } from './components/auth/principal/principal.component';
+import { PerfilComponent } from './components/auth/perfiles/perfil/perfil.component';
+import { NuevoUsuarioComponent } from './components/public/nuevo-usuario/nuevo-usuario.component';
+import { LoginComponent } from './components/public/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PerfilesComponent } from './components/auth/perfiles/perfiles.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent},
+  { path: 'nuevoUsuario', component: NuevoUsuarioComponent},
+  { path: 'perfiles', component: PerfilesComponent},
+  { path: 'perfil', component: PerfilComponent},
+  { path: 'principal', component: PrincipalComponent},
+  { path: 'requerimientos', component: RequerimientosComponent},
+  { path: 'requerimiento', component: RequerimientoComponent},
+  { path: 'respuestaRequerimiento', component: NuevoUsuarioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
